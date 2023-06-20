fe45558174c50f4bdc706b2c623a4b1058d3f0e1
Nishant Pandey <118831586+nishantp22@users.noreply.github.com>
2023-06-20 19:16:27 +0530

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -8,9 +8,9 @@ int main(){
     D[0]=1;
     D[1]=0;
     for(long long i=2;i<=n;i++){
-        long long d=D[i-1]+D[i-2];
+        long long d=D[i-1]+D[i-2];        //using DP to calulate D[i] from D[i-1] and D[i-2]
         d=(i-1)*d;
         D[i]=d;
     }
-    cout<<D[n]+n*D[n-1];
+    cout<<D[n]+n*D[n-1];            //Final answer
 }
